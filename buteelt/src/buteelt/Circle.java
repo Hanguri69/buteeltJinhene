af35b913dffc00854a4ab4ca4e3ea98ef289a305
User <User@DESKTOP-1P90H9H>
2024-01-30 16:05:57 +0800

    iwef

--- buteelt/src/buteelt/Circle.java
+++ buteelt/src/buteelt/Circle.java
@@ -9,7 +9,7 @@ public class Circle {
  }
  public static void main(String[] args) {
  System.out.println("------------------------------------------------------------");
- System.out.println("CircleCalc v1.0");
+ System.out.println("CircleCalc v1.000");
  System.out.println();
  System.out.println("Calculates and prints information for a user-supplied radius");
  System.out.println("------------------------------------------------------------");
